c463310f838b7bbebbae87ebb806c671576f5213
Ledyam Ramos <82295064+ledyam@users.noreply.github.com>
2024-09-08 04:24:14 -0400

    Update Database
    Arreglos Consistentes en la Base de Datos

--- Objects/objeto_fisico.gd
+++ Objects/objeto_fisico.gd
@@ -1,13 +1,13 @@
 extends Objeto
 class_name ObjetoFisico
 @onready var sprite_2d: Sprite2D = $Sprite2D
-@onready var animation_player: AnimationPlayer = $Marker2D/AnimationPlayer
+@onready var animation_player: AnimationPlayer = $Marker2D/Object_Animation
 
 
 
 func _ready() -> void:
 	randomize()
-	sprite_2d.texture = load(Stats["texture"])
+	sprite_2d.texture = load(Stats["Texture"])
 	animation_player.play("Caida_Drop")
 	
 	
