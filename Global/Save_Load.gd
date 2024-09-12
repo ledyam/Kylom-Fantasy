8cc83f4980b9101ea3f43a761f3be4d8dbd1fcb4
Ledyam Ramos <82295064+ledyam@users.noreply.github.com>
2024-09-12 15:11:07 -0400

    Create Save_Load.gd

---
+++ Global/Save_Load.gd
@@ -0,0 +1,10 @@
+extends Node
+var save_path = "user://Guardado1.tscn"
+
+# Called when the node enters the scene tree for the first time.
+
+
+func Save_Data(scene: Node):
+	pass
+func Load_Data():
+	pass
