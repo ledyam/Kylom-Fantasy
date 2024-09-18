493cd0b810827356a3edc03d7d1df951520131bd
Ledyam Ramos <82295064+ledyam@users.noreply.github.com>
2024-09-17 21:48:50 -0400

    Fixed Player
    Optimizado el sistema de Vida y Exp para evitar sobrecarga en Script de Player_UI

--- Data/Player/classPlayer.gd
+++ Data/Player/classPlayer.gd
@@ -4,9 +4,9 @@ class_name Player
 #Variables Atributos Principales Genéricos
 var current_level : int = 1 
 const MAX_LEVEL : int = 20
-var Label_Name_Level = load("res://UI/Indicadores/indicador_level.tscn")
-var vida_actual : float  = 100
-var vida_Max : float = 100 
+
+
+
 
 #Variables STATS
 var Fuerza  : int = 5
@@ -16,27 +16,13 @@ var Carisma : int = 5
 #PlUS
 var numero_flotante : PackedScene = load("res://UI/Indicadores/numero_flotante.tscn")
 
-
 #region SEÑALES
-signal Lose_Life (life: float)
-signal Give_life(life:float)
 signal Give_EXP (exp : int )
-
 #endregion
  
-func _ready() -> void:
-	CentralSignal.connect("UsarObjeto",on_RecibirVida)
-
-
-
-
-
-func on_RecibirVida(vida):
-	vida_actual += vida
-	if vida_actual <= vida_Max:
-		print(vida_actual)
-		Give_life.emit(vida_actual)
-	else : 
-		vida_actual = vida_Max
-		Give_life.emit(vida_actual)
-		
+func spawn_numero_flotante(damage): 
+	var number = numero_flotante.instantiate()
+	number.position = global_position
+	number.find_child("Label").text = "%.2f" % damage
+	number.find_child("AnimationPlayer").play("normal")
+	get_tree().current_scene.add_child(number)
--- Data/Player/player.gd
+++ Data/Player/player.gd
@@ -5,19 +5,40 @@ class_name MainPlayer
 #region VARIABLES ESTADISTICAS
 # Variables Generales
 const NOMBRE = "Marcus"
-
+var vida_actual : float  = 100 :
+	set(value):
+		if value < 0 : 
+			vida_actual = value
+			player_ui.progress_bar.value = value
+			player_ui.indicador_vida.text = str(value) + "|" + str(vida_Max)
+		else : 
+			player_ui.indicador_vida.text = str(0) + "|" + str(vida_Max)
+		
+var current_Exp : float  : 
+	set(value):
+		current_Exp = value 
+		player_ui.progress_bar_exp.value = value
+		player_ui.indicador_exp.text = str(value) + "|" + str(Max_Exp) 
+var Max_Exp : float = 50:
+	set(value):
+		Max_Exp = value
+		player_ui.progress_bar_exp.max_value = value
+var vida_Max : float = 100 :
+	set(value):
+		vida_Max = value
+		player_ui.progress_bar_exp.max_value = value
+		
 var ATK :  float = 100
 var DEF : float = 20
-
-
 #endregion
 
 #region VARIABLES De Control
+
 var can_attack : bool = true
 var can_move : bool = true 
 var direction : PlayerDirections = PlayerDirections.new()
 var recibir_damage = false
-var cda = false
+var cooldown_Rdamage = false
 var knockback = Vector2.ZERO
 #endregion
 
@@ -31,8 +52,6 @@ var knockback = Vector2.ZERO
 @onready var player_ui: Control = $"Player_UI"
 @onready var ui_menu_in_game: Control = $UI_Menu_InGame
 @onready var indicador_nombre_level: Label = $Indicador_nombre_level
-
-
 #endregion
 
 #region CONSTANTES 
@@ -44,26 +63,30 @@ const states : Dictionary = {
 	'_dead' : "PlayerDead"
 }
 const animations : Dictionary = {
-	'_idle': 'Player_idle',
-	'_idle_turnL':'Player_idle_turn_left',
-	'_idle_turnR':'Player_idle_turn_right',
-	'_idle_up':'Player_idle_up',
-	'_walkr': 'Player_walk_right',
-	'_walkl': 'Player_walk_left',
-	'_walk_down':'Player_walk_down',
-	'_walk_up':'Player_walk_up',
-	'_attack':'Player_attack_down',
-	'_attack_up':'Player_attack_up',
-	'_attack_turnL':'Player_attack_turn_left',
-	'_attack_turnR':'Player_attack_turn_right',
-	'_dead' : 'Player_dead'
+	
+	_idle = 'Player_idle',
+	_idle_turnL = 'Player_idle_turn_left',
+	_idle_turnR = 'Player_idle_turn_right',
+	_idle_up = 'Player_idle_up',
+	_walkr =  'Player_walk_right',
+	_walkl =  'Player_walk_left',
+	_walk_down = 'Player_walk_down',
+	_walk_up = 'Player_walk_up',
+	_attack = 'Player_attack_down',
+	_attack_up = 'Player_attack_up',
+	_attack_turnL = 'Player_attack_turn_left',
+	_attack_turnR = 'Player_attack_turn_right',
+	_dead  = 'Player_dead'
 }
 
 #endregion
 
 
 #region FUNCIONES del Engine
-
+func _ready() -> void:
+	CentralSignal.connect("UsarObjeto",on_RecibirVida)
+	
+	
 func _physics_process(_delta: float) -> void:
 	move_and_slide()
 
@@ -71,32 +94,36 @@ func _physics_process(_delta: float) -> void:
 #endregion
 
 #region MÉTODOS del Player
-func Recibir_damage(enemy_Atack : float):
-	if recibir_damage and cda:
-		vida_actual-= enemy_Atack
-		Lose_Life.emit(vida_actual)
-		spawn_numero_flotante(enemy_Atack)
-		cda = false 
+func Recibir_damage(enemy_attack : float):
+	if recibir_damage and cooldown_Rdamage:
+		vida_actual-= enemy_attack
+		spawn_numero_flotante(enemy_attack)
+		cooldown_Rdamage = false 
 		cd.start()
 	pass
 	
-func Give_Exp(exp : int ): 
-	Give_EXP.emit(exp)
 	
+func on_RecibirVida(vida):
+	vida_actual += vida
+	
+func Give_Exp(exp : int ): 
+	if(player_ui.progress_bar_exp.value + exp) > player_ui.progress_bar_exp.max_value:
+		var resto = (player_ui.progress_bar_exp.value + exp) - player_ui.progress_bar_exp.max_value
+		LEVEL_UP()
+		Max_Exp = LocalDatabase.content[str(current_level)]["Exp_Necesaria"]
+		current_Exp = 0 
+		current_Exp += resto
+	else : 
+		current_Exp += exp
+		
+
 func LEVEL_UP ():
 	current_level += 1
 	vida_Max += 10
 	vida_actual = vida_Max
-	player_ui.Actualizar_Label_UI_Life()
 	$"Sounds/LevelUpPickup(rpg)".play()
 	indicador_nombre_level.Actualizar()
-	
-func spawn_numero_flotante(damage): 
-	var number = numero_flotante.instantiate()
-	number.position = global_position
-	number.find_child("Label").text = "%.2f" % damage
-	number.find_child("AnimationPlayer").play("normal")
-	get_tree().current_scene.add_child(number)
+
 
 
 #region ALERTA DE SEÑALES
@@ -109,7 +136,7 @@ func _on_timer_timeout() -> void:
 
 #TIMERS
 func _on_cd_timeout() -> void:
-	cda = true
+	cooldown_Rdamage = true
 	recibir_damage = false
 	velocity = Vector2.ZERO
 	pass 
@@ -122,7 +149,7 @@ func _on_cd_timeout() -> void:
 func _on_hit_box_body_entered(body: Node2D) -> void:
 	if body as EnemyOriginal and vida_actual > 0:
 		recibir_damage = true
-		cda = true
+		cooldown_Rdamage = true
 #Asiganción de Empuje al Recibir Daño
 		knockback = (self.global_position - body.position).normalized() 
 		velocity = knockback * 20
@@ -137,7 +164,7 @@ func _on_hit_box_body_entered(body: Node2D) -> void:
 func _on_hit_box_body_exited(body: Node2D) -> void:
 	if body.name == "Skeleton":
 		recibir_damage = false
-		cda = false 
+		cooldown_Rdamage = false 
 	pass 
 #Señal para Ofertar Daño
 func _on_damage_box_body_entered(body: Node2D) -> void:
