60f871cb9da41a7fbe45abbe277d43c36e0ebe62
Ledyam Ramos <82295064+ledyam@users.noreply.github.com>
2024-09-27 14:24:33 -0400

    Other

--- Data/Player/PlayerStates/player_dead.gd
+++ Data/Player/PlayerStates/player_dead.gd
@@ -4,6 +4,7 @@ func enter ():
 	print ("State >>>>DEADDDD<<<<")
 	player.velocity = Vector2.ZERO
 	player.animated_player_movement.play(player.animations._dead)
+	
 	timer.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
