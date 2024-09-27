64b40b4d544ecae261e31cf52a825950234f3778
Ledyam Ramos <82295064+ledyam@users.noreply.github.com>
2024-09-27 14:22:31 -0400

    Update Objeto
     Cambios en la Implementación del Nodo Objeto

--- Data/Objects/objeto_fisico.gd
+++ Data/Objects/objeto_fisico.gd
@@ -1,17 +1,20 @@
 extends Objeto
+
 class_name ObjetoFisico
-@onready var sprite_2d: Sprite2D = $Sprite2D
+
+
 @onready var animation_player: AnimationPlayer = $Marker2D/Object_Animation
 
 
 
 func _ready() -> void:
 	randomize()
-	sprite_2d.texture = load(Stats["Texture"])
+	self.texture = load(Stats["Texture"])
 	animation_player.play("Caida_Drop")
 	
-	
 
-
-	
-	
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body as MainPlayer :
+		body.ui_menu_in_game.inventario.add_item(Stats)
+		queue_free()
+	pass # Replace with function body.
