a07432f5fb014b42a302ec25f310ab468e044467
Ledyam Ramos <82295064+ledyam@users.noreply.github.com>
2024-12-27 23:34:57 -0500

    Fix SlotBestiary
    Arreglado cuando el jugador hacia click en Slot del bestiario y mostraba animación de mob no desbloqueado

--- UI/UI_Interfaces/slot_bestiary.gd
+++ UI/UI_Interfaces/slot_bestiary.gd
@@ -20,6 +20,7 @@ var mob_name : String
 #Accion de usar objeto Consumible del Inventario 
 func _on_gui_input(event: InputEvent) -> void:
 	if event.is_action_pressed("click"):
+		if !is_vacio:
 			owner.find_child("Name").text = mob_name
 			owner.find_child("Description").text = " Un " + mob_name + " culero"
 			owner.find_child("AnimatedSprite2D").play(mob_name)
